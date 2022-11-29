e4de051fe9e458cf4a8f7141b0256a7a1a865b40
Kushan jayasinghe <kushanace@gmail.com>
2022-11-29 12:36:19 +0530

    Initialize DAO Layer

--- src/main/resources/db-scipt.sql
+++ src/main/resources/db-scipt.sql
@@ -1,9 +1,11 @@
 
-CREATE TABLE user(
+CREATE TABLE IF NOT EXISTS user(
     full_name VARCHAR(256) NOT NULL ,
     user_name VARCHAR(200)  PRIMARY KEY ,
     password VARCHAR(200) NOT NULL
 );
 CREATE TABLE to-do-item (
+    id
+
 
 )
